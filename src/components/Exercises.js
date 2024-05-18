e46e8a2eb7250f5c56be97db932629b40d160251
UdaySingh108 <singh.uday2610@gmail.com>
2024-05-18 11:40:48 +0530

    Update Exercise.js

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -20,7 +20,7 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
         exercisesData = await fetchData(`https://exercisedb.p.rapidapi.com/exercises/bodyPart/${bodyPart}`, exerciseOptions);
       }
 
-      setExercises(exercisesData);
+      setExercises([exercisesData]);
     };
 
     fetchExercisesData();
